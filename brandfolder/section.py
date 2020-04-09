d158087034cb6f8a381bba2bcccccd0c2ad9c46b
Brett Nekolny <brett.nekolny@brandfolder.com>
2020-04-09 11:39:31 -0600

    Adding circleci config, pep8 checks, and fixing some pep8 errors.

--- brandfolder/section.py
+++ brandfolder/section.py
@@ -2,6 +2,7 @@ from brandfolder.resource import Resource
 from brandfolder.resource_container import ResourceContainer
 from brandfolder.asset import Asset
 
+
 class Section(Resource):
     def __init__(self, client, data):
         super().__init__(client, data, 'Section', 'sections')
