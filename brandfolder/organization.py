3ef080b47053c720469d13ada0f34d25afefa027
Kevin Schwindt <schwindt.kevin@gmail.com>
2020-07-20 18:58:07 -0600

    add /organizations/id/attachments

--- brandfolder/organization.py
+++ brandfolder/organization.py
@@ -2,6 +2,7 @@ from brandfolder.resource import Resource
 from brandfolder.resource_container import ResourceContainer
 from brandfolder.brandfolder import Brandfolder
 from brandfolder.asset import Asset
+from brandfolder.attachment import Attachment
 
 
 class Organization(Resource):
@@ -12,6 +13,7 @@ class Organization(Resource):
         super().__init__(client, **kwargs)
 
         self.assets = ResourceContainer(client, Asset, parent=self)
+        self.attachments = ResourceContainer(client, Attachment, parent=self)
 
     def __repr__(self):
         return f'<{self.resource_name} {self.attributes["slug"]}: {self.id}>'
